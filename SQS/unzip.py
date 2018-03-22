c8bf3771e4e139fffa65f10424465c8cf0902acc
Samuel Hausmann <samhausmann@Samuels-MacBook-Air.local>
2018-03-22 18:50:39 -0400

    downloading and decompressing 7z files in unzip.py

--- SQS/unzip.py
+++ SQS/unzip.py
@@ -1,10 +1,13 @@
 import boto3
 import re
+import pylzma
+import os
 import sys
 
 sqs = boto3.client('sqs')
 url = sqs.get_queue_url(QueueName='sample_site_queue')
 
+i = 0
 while sqs.get_queue_attributes(QueueUrl=url['QueueUrl'], AttributeNames=['ApproximateNumberOfMessages']) > 1:
 	response = sqs.receive_message(
     QueueUrl=url['QueueUrl'],
@@ -20,6 +23,13 @@ while sqs.get_queue_attributes(QueueUrl=url['QueueUrl'], AttributeNames=['Approx
 		receipt_handle = message['ReceiptHandle']
 
 		print message
+
+		os.system('wget https://ia800107.us.archive.org/27/items/stackexchange/' + message['Body'])
+		os.system('7z x ' + message['Body'] + ' -o' + message['MessageAttributes']['Site']['StringValue'])
+
+		# put dir into S3
+
+		sys.exit()
 
 		sqs.delete_message(QueueUrl=url['QueueUrl'], ReceiptHandle=receipt_handle)
 	else:
